263aa901c2ef50fca344f61beacafa3c6f0ff4d0
Michael Seghers <michael.seghers@ida-mediafoundry.be>
2013-03-28 00:54:38 +0100

    Added remoting to the app.

--- Big Bang FX/src/devoxxfrance/SampleController.java
+++ Big Bang FX/src/devoxxfrance/SampleController.java
@@ -7,11 +7,13 @@ package devoxxfrance;
 import devoxxfrance.model.Person;
 import devoxxfrance.view.PersonCell;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -25,7 +27,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Callback;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  *
@@ -60,40 +69,40 @@ public class SampleController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        /*try {
-            URL personUrl = new URL("http://idamf-restdemo.herokuapp.com/app/persons.json");
-            HttpURLConnection conn = (HttpURLConnection) personUrl.openConnection();
-            
-            System.out.println(conn.getContent());
-            
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
         
-        
-        Person a = new Person();
-        a.setName("Leonard");
-        a.setLastName("Hofstadter");
-        a.setProfession("Experimental physicist");
-        a.setRealName("Johnny Galecki");
-        a.setBio("Leonard Leakey Hofstadter, Ph.D., is a fictional character on the CBS television series The Big Bang Theory, portrayed by actor Johnny Galecki. Leonard is an experimental physicist originally from New Jersey who shares an apartment with colleague and friend Dr. Sheldon Cooper (Jim Parsons). Leonard and Sheldon are named after actor/producer Sheldon Leonard, and Nobel Prize Laureates Robert Hofstadter and Leon Cooper. Leonard has been described as the straight man of the series. Penny (Kaley Cuoco) is Leonard's next-door neighbor and main love interest, and the teasing of romance between the two of them is a major force driving the series. For his portrayal, Galecki was nominated for a Primetime Emmy Award and a Golden Globe Award.");
-        a.setImageName("Leonard_Hofstadter.jpg");
-        a.setWebsiteUrl("http://en.wikipedia.org/wiki/Johnny_Galecki");
-        
-        Person b = new Person();
-        b.setName("Howard");
-        b.setLastName("Wollowitz");
-        b.setProfession("Aerospace Engineer");
-        b.setRealName("Simon Helberg");
-        b.setBio("Howard Joel Wolowitz, M.Eng is a fictional character on the CBS television series The Big Bang Theory, portrayed by actor Simon Helberg. Among the main male characters in the show, Howard is distinguished for lacking a doctoral degree, for still living with his mother, and for believing himself to be a \"ladies' man\". Simon Helberg's character is named after a computer programmer known by the show's co-creator Bill Prady.");
-        b.setImageName("Howard_Wolowitz.jpg");
-        b.setWebsiteUrl(null);
-        
-        
-        ObservableList<Person> persons = FXCollections.observableArrayList(a, b);
-        listView.setItems(persons);
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpGet get = new HttpGet("http://idamf-restdemo.herokuapp.com/app/persons.json");
+        ObservableList<Person> persons = null;
+        try {
+            ResponseHandler<ObservableList<Person>> responseHandler = new ResponseHandler<ObservableList<Person>>() {
+
+                @Override
+                public ObservableList<Person> handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+                    ObjectMapper mapper = new ObjectMapper();
+                    Map<String, List<Map<String, String>>> map =  mapper.readValue(response.getEntity().getContent(), Map.class);
+                    List<Map<String, String>> array = map.get("personList");
+                    Person p;
+                    List<Person> personsAL = new ArrayList<Person>();
+                    for (Map<String, String> pMap : array) {
+                        p = new Person();
+                        p.setId(pMap.get("id"));
+                        p.setName(pMap.get("name"));
+                        p.setLastName(pMap.get("lastName"));
+                        p.setProfession(pMap.get("profession"));
+                        p.setRealName(pMap.get("realName"));
+                        p.setWebsiteUrl(pMap.get("url"));
+                        p.setImageName(pMap.get("imageUri"));
+                        p.setBio(pMap.get("bio"));
+                        personsAL.add(p);
+                    }
+                    return FXCollections.observableArrayList(personsAL);
+                }
+            };
+            persons = httpclient.execute(get, responseHandler);
+            listView.setItems(persons);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         
         listView.setCellFactory(new Callback<ListView<Person>, ListCell<Person>>() {
           public ListCell<Person> call(ListView<Person> param) {
